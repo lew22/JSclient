dcedf51bb1accb6e6a2b5e1df04152a282162629
lew22 <richardgarmori@gmail.com>
2022-06-26 11:42:51 -0500

    instructions to terminal

--- client/client.js
+++ client/client.js
@@ -18,20 +18,3 @@ client.on('end', function() {
    console.log('disconnected from server');
 });
 
-//socket sent
-
-let socket = new net.Socket();
-
-socket.connect(port, host, () => {
-
-    socket.write("hola");
-    
-});
-
-socket.on('data', data => {
-    console.log(`${data}`);
-  });
-  
-socket.on('close', () => {
-    socket.destroy();
-  });
